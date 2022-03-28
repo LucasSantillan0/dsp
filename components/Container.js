9920c34ebed78533712100a889fc908f1eef6005
Lucas santillan <lucas.santillan@devsu.com>
2022-03-27 21:36:01 -0300

    logo

--- components/Container.js
+++ components/Container.js
@@ -1,7 +1,16 @@
+import Image from "next/dist/client/image";
+import logo from '../producto0/logo-removebg-preview.png'
 export default function Container({ children, className }) {
   return (
     <div className="bg-slate-200 w-screen h-screen absolute top-0">
-      <div className={`md:rounded-md md:flex md:w-8/12 bg-white w-full  md:h-5/6 mx-auto mt-16 rounded-t-3xl h-full ${className}`}>
+      <div className= " flex justify-center items-center my-3">
+      <Image
+      src={logo}
+      alt='logo'
+      width={70}
+      height={70}
+      /></div>
+      <div className={`md:rounded-md md:flex md:w-8/12 bg-white w-full  md:h-5/6 mx-auto rounded-t-3xl h-full ${className}`}>
         {children}
       </div>
     </div>
